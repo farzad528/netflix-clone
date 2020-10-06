4d5d182bc6a0284e7a21915182249141017b25d6
Farzad Sunavala <farzadsunavala@chevron.com>
2020-10-05 21:59:26 -0500

    got data!

---
+++ src/Row.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  // A snippet of code which runs based on a specific condition/variable
+  useEffect(() => {
+    // if brackets are empty, run once when the row loads, and don't run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log(request);
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>{title}</h2>
+
+      {/* {container -> posters} */}
+    </div>
+  );
+}
+
+export default Row;
